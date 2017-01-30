4b9e3270ca5a4fe7299ae993db34bccd4b2b5be6
QuentinR <quentin.rouhier@gmail.com>
2017-01-30 11:58:54 +0100

    addition et multiplication en php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>php</title>
+  </head>
+  <body>
+    <?php
+    				$nombre = 8;
+    				$nombres = 47;
+
+    				echo $resultat=$nombre+$nombres  ;
+    				echo "<br/>";
+    				echo $resultat=$nombres*$nombre;
+    ?>
+  </body>
+</html>
